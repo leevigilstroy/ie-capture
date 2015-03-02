c08c64864280439aa67750a0ac86104e49fba97f
leevigilstroy <oliverstigley@gmail.com>
2015-03-02 19:33:59 +0000

    Basic User Controller Added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,38 @@
 class UsersController < ApplicationController
   
   def new
+    @user = User.new
   end
   
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to(:action => 'show', :id => @user.id)
+    else
+      redirect_to(:action => 'new')
+    end
+    debugger
+  end
+  
+  def index
+    @users = Users.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   def edit
+    @user = User.find(params[:id])
   end
   
-  def update
+  def update    
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+        redirect_to(:action => 'show', :id => @user.id)
+       else
+      redirect_to(:action => 'edit')
+       end
   end
   
   def delete
@@ -19,6 +42,9 @@ class UsersController < ApplicationController
   end
   
   private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :mortgage_reference_num)
+  end
   
   
 end
